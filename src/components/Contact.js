9611df26fc2640a11f5bb2382b9bd0d5d4e43d2f
Calvin Cheung <calvchum@gmail.com>
2019-05-02 19:17:12 +1000

    removed Location component from Contact.js, need to import Constants and map through it

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Location from './Location';
 
 const Contact = () => {
 	return (
     <div>
       <h1>Contact page</h1>
       <p>MAP THROUGH ARRAY CONTAINING LOCATIONS AND DISPLAY THEM WITH LOCATION COMPONENT</p>  
-      <Location/>
     </div>
 	)
 };
