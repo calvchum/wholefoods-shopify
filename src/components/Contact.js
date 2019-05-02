24dfa859a2843867f42344a0aa0b7ddeccc0b8e8
Calvin Cheung <calvchum@gmail.com>
2019-05-02 19:21:08 +1000

    updated Contact.js with Constants.locationData.map (same as Footer) but needs to have a different colour for the white background

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import * as Constants from '../Constants/locationInfo';
+import Location from './Location'
 
 const Contact = () => {
+
+  let locations = Constants.locationData.map((location, i) => {
+    return (
+      <Location
+        key={i}
+        location={location}
+      />
+    );
+  });
+
 	return (
     <div>
       <h1>Contact page</h1>
-      <p>MAP THROUGH ARRAY CONTAINING LOCATIONS AND DISPLAY THEM WITH LOCATION COMPONENT</p>  
+      {locations}
     </div>
 	)
 };
