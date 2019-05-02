434e758eeb04d6160b4ef5133a1861a000c6bc92
Ben Quartermaine <ben.quartermaine3@gmail.com>
2019-05-02 18:25:04 +1000

    added footer mobile compatbility, styled the products page tp render in grid form, with filters to the left handside. Next step is to detail the product cards

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,7 @@ class Product extends Component {
   render(){
     return (
       <div>
-        <img src={this.props.product.images[0].src} alt="" height="150px"/>
+        <img src={this.props.product.images[0].src} alt="" height="217px" width="227"/>
       </div>
     )
   }
