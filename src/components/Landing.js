90dacb7cd7260820980c1f07ab957f7659704665
Ben Quartermaine <ben.quartermaine3@gmail.com>
2019-05-03 19:22:42 +1000

    landing page styled, changed the landing page to a class

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Location from './Location';
+import * as Constants from '../Constants/locationInfo';
 
-const Landing = () => {
-	return (
-    <div className="landing-wrapper">
-      <h1>Wholefoods landing page</h1>
-      <p>Welcome to Organic Wholefoods. A Melbourne-based ethically sourced, organic and wholefood focused store.
+class Landing extends Component {
+  render() {
+    let locations = Constants.locationData.map((location, i) => {
+      return (
+        <Location
+          key={i}
+          location={location}
+        />
+      );
+    });
 
-      We stock a heap of great products, all trying to do better by our planet and we can have a wide range of different services we offer, from fresh cut peanut butter, soap refills and more. Head on over to the offerings page to get a full list.</p>  
-      <Link to={'products'}>
-        <button className="browse-button cta-button">
-          Browse Products
-        </button>
-      </Link>
-    </div>
-	)
-};
+    return (
+      <div className="landing-wrapper">
+      <div className="landing-bkg">
+        <div className="landing-copy">
+          <p className="body-text">Welcome to Organic Wholefoods. A Melbourne-based ethically sourced, organic and wholefood focused store.
+          </p>
+          <p className="body-text">
+          We stock a heap of great products, all trying to do better by our planet and we have a wide range of different services we offer, from fresh cut peanut butter, soap refills and more. Head on over to the offerings page to get a full list.
+          </p>  
+          <Link to={'products'}>
+            <button className="browse-button cta-button">
+              Browse Products
+            </button>
+          </Link>
+      </div>
+        </div>
+        <div className="landing-locations">
+          {locations}
+        </div>
+        <div className="landing-instagram">
+        </div>
+      </div>
+    )
+  }
+}
+
 
 export default Landing; 
