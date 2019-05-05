5bc06c0915f5aaa994641aa0fd0371bd5d789e06
Calvin Cheung <calvchum@gmail.com>
2019-05-06 01:34:41 +1000

    added the quantityField field and selectedVariantQuantity to state

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -12,6 +12,7 @@ class SingleProduct extends Component {
     this.state = { selectedOptions: defaultOptionValues };
 
     this.handleOptionChange = this.handleOptionChange.bind(this);
+    this.handleQuantityChange = this.handleQuantityChange.bind(this);
   }
 
   handleOptionChange(event) {
@@ -26,12 +27,32 @@ class SingleProduct extends Component {
     });
   } 
 
+  handleQuantityChange(event) {
+    this.setState({
+      selectedVariantQuantity: event.target.value
+    });
+  }
+
   render(){
+    //ES6 refactoring 
     const { title, images, options } = this.props.product
+    // defining te variant variables
+    let variantImage = this.state.selectedVariantImage || this.props.product.images[0]
+    let variant = this.state.selectedVariant || this.props.product.variants[0]
+    let variantQuantity = this.state.selectedVariantQuantity || 1
+    // the Add To Cart button is coded here 
+    const addToCartButton = <button onClick={ () => console.log(variant)} className="add-to-cart">Add To Cart</button>
+    // the quantity field is coded here
+    const quantityField = <label> 
+      Quantity
+      <input min="1" type="number" defaultValue={variantQuantity} onChange={this.handleQuantityChange}></input>
+    </label>
+
     let variantSelectors = options.map((option) => {
       return (
         <VariantSelector
           handleOptionChange={this.handleOptionChange}
+          handleQuantityChange={this.handleQuantityChange}
           key={option.id.toString()}
           option={option}
         />
@@ -41,8 +62,13 @@ class SingleProduct extends Component {
     return (
       <div>
         <img src={images[0].src} height="300px" alt=""/>
+        <br />
         {title}
+        <br />
+        {quantityField}
+        <br />
         {variantSelectors}
+        {addToCartButton}
       </div>
     )
   }
