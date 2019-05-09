b45432edf7a8de5c0ff62ef4a2550345ca954c94
Ben Quartermaine <ben.quartermaine3@gmail.com>
2019-05-09 10:19:40 +1000

    DEBUGGED

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -19,24 +19,24 @@ class ProductGrid extends Component {
     return (
       <div className="product-wrapper">
         <div className="left-side filters">
-        <div className="filter-icon">
-          <img src={ require('../assets/burger-menu.svg') } alt="Burger menu"/>
+          <div className="filter-icon">
+            <img src={ require('../assets/burger-menu.svg') } alt="Burger menu"/>
+          </div>
+          <ul>
+            <li>Brand</li>
+            <li>Diet</li>
+            <li>Category</li>
+            <li>Specials</li>
+            <li>Bulk Buys</li>
+          </ul>
         </div>
-        <ul>
-          <li>Brand</li>
-          <li>Diet</li>
-          <li>Category</li>
-          <li>Specials</li>
-          <li>Bulk Buys</li>
-        </ul>
-        </div>
+
         <div className="right-side">
           <p>Showing results for 'Search query'</p>
-          <div className="product">  
-            {products}
-          <p>{search !== '' ? `Showing results for '${search}'...` : 'Showing all products...'}</p>
           <div className="products">
             <div className="product">   
+            {allProducts}
+          <p>{search !== '' ? `Showing results for '${search}'...` : 'Showing all products...'}</p>
               {search !== '' ? this.props.searchResults.map((product) => { return ( <Product client={this.props.client} key={product.id.toString()} product={product}/> )}) : allProducts}
             </div>
           </div>
