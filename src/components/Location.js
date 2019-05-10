8205b116eff037584a22470b991540b06b8ee3d2
Ben Quartermaine <ben.quartermaine3@gmail.com>
2019-05-10 15:52:00 +1000

    refactoring locations

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,33 +1,31 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+
 
 class Location extends Component {
   render() {
     const data = this.props.location
     return (
-      <div className="location-wrapper">
-        <div className="location-details">
+      <Container>
+        <Row>
           <h3>{data.location}</h3>
+        </Row>
+        <Row>
           <p>{data.address}</p>
-        </div>
-        <div className="open-times">
-          <div className="day-times">
-            <span>Mon - Fri</span>
-            <span>{data.hoursMTF}</span>
-          </div>
-          <div className="day-times">
-            <span>Saturday</span>
-            <span>{data.hoursSat}</span>
-          </div>
-          <div className="day-times">
-            <span>Sunday</span>
-            <span>{data.hoursSun}</span>
-          </div>
+        </Row>
+        <Row>
+            <Col xs="6"><span>Mon - Fri</span></Col>
+            <Col xs="6"><span>{data.hoursMTF}</span></Col>
 
-          <div className="phone-number">
-            <span>{data.phone}</span>
-          </div>
-        </div>
-      </div>
+            <Col xs="6"><span>Saturday</span></Col>
+            <Col xs="6"><span>{data.hoursSat}</span></Col>
+
+
+            <Col xs="6"><span>Sunday</span></Col>
+            <Col xs="6"><span>{data.hoursSun}</span></Col>
+          <span>{data.phone}</span>
+        </Row>
+      </Container>
     );
   };
 };
