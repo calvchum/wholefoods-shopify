08ad2180abfe0e4b634cb1129805dfcd1f747ec7
Calvin Cheung <calvchum@gmail.com>
2019-05-16 19:02:24 +1000

    replicated core functionality of Searchbar into GridSearch, removed the onClick features

--- src/components/GridSearch.js
+++ src/components/GridSearch.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Searchbar extends Component {
@@ -7,20 +6,7 @@ class Searchbar extends Component {
     super(props);
 
     this.handleClick = this.handleClick.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-    this.state = {
-      clicked: false
-    }
   }
-
-  handleClick(e) {
-    this.setState({
-      clicked: !this.state.clicked
-    });
-    this.focus.focus();
-    e.preventDefault();
-  };
 
   handleChange(e) {
     const search = e.target.value
@@ -28,27 +14,13 @@ class Searchbar extends Component {
     this.props.updateSearchResults();
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.history.push('/products/');
-  }
-
   render() {  
-    let clickedStatus = this.state.clicked ? 'showSearch' : 'noShowSearch';
-
     return (
       <div className="search-wrapper">
-        <div onClick={(e) => {this.handleClick(e)}} className='search-icon-wrapper'>
-          <a href='#'>
-            <img src={ require("../assets/search-icon.svg") } alt=""/>
-          </a>
-        </div>
-        <div className={`search-bar ${clickedStatus}`}>
-        <form action="" onSubmit={(e) => {this.handleSubmit(e)}}>
+        <div className={`search-bar`}>
+        <form action=""}>
           <input
-            ref={(focus) => { this.focus = focus; }}
             type="text"
-            clicked={clickedStatus}
             placeholder='Search..'
             defaultValue=''
             onKeyUp={(e) => {this.handleChange(e)}}            
